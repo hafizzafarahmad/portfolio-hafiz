f76ac796bc684086dbc4850054b61bd3a02ff1f0
Hafiz <hafizzafarahmad@gmail.com>
2024-04-13 21:27:43 +0700

    [update] info

--- store/profile.js
+++ store/profile.js
@@ -118,78 +118,7 @@ export const state = () => ({
       },
     ],
     portfolio: [
-      {
-        name: "Diary App",
-        pictures: [
-          {
-            img: require("@/assets/portfolio/diary/1.png"),
-          },
-          {
-            img: require("@/assets/portfolio/diary/2.png"),
-          },
-          {
-            img: require("@/assets/portfolio/diary/3.png"),
-          },
-        ],
-        technologies: ["react", "scss", "local storage"],
-        category: "Web App",
-        date: "Aug, 2021 - 20 days",
-        github: "https://github.com/mahy209/notes-react-app.git",
-        visit: "https://github.com/mahy209/notes-react-app.git",
-        description:
-          " Diary react app with some new features: Dark mode, upload images, tags, filter search with title/content/tag ",
-      },
-      {
-        name: "Online Examination",
-        pictures: [
-          {
-            img: require("@/assets/portfolio/online examination/exam (1).png"),
-          },
-          {
-            img: require("@/assets/portfolio/online examination/exam (2).png"),
-          },
-          {
-            img: require("@/assets/portfolio/online examination/exam (3).png"),
-          },
-          {
-            img: require("@/assets/portfolio/online examination/exam (4).png"),
-          },
-          {
-            img: require("@/assets/portfolio/online examination/exam (5).png"),
-          },
-        ],
-        technologies: ["HTML", "CSS", "JavaSript", "JQuery", "PHP", "MySQL"],
-        category: "Web App",
-        date: "Apr, 2021 - May, 2021",
-        github: "https://github.com/mahy209/online-examination.git",
-        visit: "https://github.com/mahy209/online-examination.git",
-        description:
-          "Online Examination System Today Online Examination System has become a fast growing examination method because of its speed and accuracy. It is also needed less manpower to execute the examination. Almost all organizations now-a-days, are conducting their objective exams by online examination system, it saves students time in examinations. Organizations can also easily check the performance of the student that they give in an examination. As a result of this, organizations are releasing results in less time. It also helps the environment by saving paper. According to today’s requirement, online examination project in php is very useful to learn it. What is an online examination system? In an online examination system examine get their user id and password with his/her admit card. This id is already saved in the examination server. When examine login to the server he/she get his/her profile already register. On the certain time examine gets the message to start the examination. All answers given by examine are saved into the server with his/her profile information. Online examination system also allows to correct the answer if the examine needed to change any answer in the examination time duration, however, after the time duration any change will not allow. This also makes c checking the answer easy and error proof as computers are more accurate than man and provide fast results too. Php is a web base language so we can create an online examination system in PHP. Administrator of Online Examination has multiple features such as Add, Delete, Update Topics and Question. To Login as Admin put inside your browser.",
-      },
-      {
-        name: "MyFlix.com",
-        pictures: [
-          {
-            img: require("@/assets/portfolio/myflix.com/1.png"),
-          },
-          {
-            img: require("@/assets/portfolio/myflix.com/2.png"),
-          },
-          {
-            img: require("@/assets/portfolio/myflix.com/3.png"),
-          },
-          {
-            img: require("@/assets/portfolio/myflix.com/4.png"),
-          },
-        ],
-        technologies: ["react", "css", "TMDB"],
-        category: "Web App",
-        date: "Aug, 2021 - 10 days",
-        github: "https://github.com/mahy209/MyFlix-react-app.git",
-        visit: "https://github.com/mahy209/MyFlix-react-app.git",
-        description:
-          " flix.com is a netflix clone using React JS and TMDB with courusal and search engine. ",
-      },
+
     ],
     portfolio_design: [
       {
