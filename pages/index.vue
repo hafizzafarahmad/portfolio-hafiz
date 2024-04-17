56d73404dbf27295a32d86c3159b9c97525ec902
Hafiz <hafizzafarahmad@gmail.com>
2024-04-17 13:19:08 +0700

    [update] deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="text-center" style="overflow:hidden">
-    <b-row align-h="center" align-v="center" style="height: 100vh">
+  <!-- class="text-center" style="overflow:hidden" -->
+  <div >
+    <!-- <b-row align-h="center" align-v="center" style="height: 100vh">
       <b-col xl="3" md="4" sm="6" cols="6">
         <img src="@/assets/50168181.png" class="about-img" style="width: 100%;" />
         <div class="mt-4">
@@ -8,8 +9,8 @@
         </div>
       </b-col>
       
-    </b-row>
-    <!-- <Main></Main> -->
+    </b-row> -->
+    <Main></Main>
   </div>
 </template>
 
